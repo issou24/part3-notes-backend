b5f3ba997baec12625504e5b5ab60f3330cc272e
FIRST_NAME LAST_NAME <l.frz65@gmail.com>
2025-07-23 01:00:00 +0200

    p

---
+++ mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+// Vérifie les arguments
+if (process.argv.length < 5) {
+  console.log("Usage: node mongo.js <password> <name> <number>");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+const url = `mongodb+srv://lio3:${password}@cluster0.azarczm.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+// Schéma Mongoose
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+// Nouvelle personne à ajouter
+const person = new Person({
+  name,
+  number,
+});
+
+// Sauvegarde dans la base
+person
+  .save()
+  .then(() => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  })
+  .catch((error) => {
+    console.error("Erreur lors de l'ajout :", error.message);
+    mongoose.connection.close();
+  });
