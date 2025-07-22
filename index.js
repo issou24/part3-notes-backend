2041d1443f499de517acc9376fd812b5afe8c12d
issou24 <127043948+issou24@users.noreply.github.com>
2025-07-22 17:42:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // ou app.use(cors()) pour tout autoriser
+    origin: "https://phonebook-front-2rlkoi2lk-issou24s-projects.vercel.app/", // ou app.use(cors()) pour tout autoriser
   })
 );
 
