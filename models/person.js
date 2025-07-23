685da3af98687f18d8c95614505a107392b5a652
FIRST_NAME LAST_NAME <l.frz65@gmail.com>
2025-07-24 01:42:33 +0200

    model

--- models/person.js
+++ models/person.js
@@ -8,6 +8,12 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
+    minlength: 8,
+    required: true,
+    match: [
+      /^\d{2,3}-\d+$/,
+      'Le numéro doit être formé de deux parties séparées par un "-", la première partie contient 2 ou 3 chiffres, la deuxième partie contient des chiffres.',
+    ],
   },
 });
 
